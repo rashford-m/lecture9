fd0cce7e9539f98a6c0c0c3804d2dd0004f4c9b5
Rashford <mikearowo@gmail.com>
2023-02-09 22:30:33 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
-import { Menu } from "./pages/Menu";
 import { Navbar } from ".Navbar";
+import { useState } from "react";
 
 function App() {
+  const [username, setUsername] = useState("");
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
+          <Route path="/" element={<Home username={username} />} />
+          <Route path="/profile" element={<Profile username={username} />} />
           <Route path="/contacts" element={<Contact />} />
           <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
         </Routes>
