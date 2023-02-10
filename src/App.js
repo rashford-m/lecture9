8ce1fc0e3603316f0b5f2ab6957860a6d8a3efb1
Rashford <mikearowo@gmail.com>
2023-02-10 13:17:20 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Profile } from "./pages/Profile";
 import { Contact } from "./pages/Contact";
-import { Navbar } from ".Navbar";
-import { useState } from "react";
+import { Navbar } from "./Navbar";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
 
 function App() {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("Rashford");
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home username={username} />} />
-          <Route path="/profile" element={<Profile username={username} />} />
-          <Route path="/contacts" element={<Contact />} />
-          <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
-        </Routes>
-      </Router>
+      <AppContext.Provider value={{username, setUsername}}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home username={username} />} />
+            <Route
+              path="/profile"
+              element={
+                <Profile username={username} setUsername={setUsername} />
+              }
+            />
+            <Route path="/contacts" element={<Contact />} />
+            <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,3 @@
+export const Home = (props) => {
+    return <h1> This is the Home-Page and user is: {props.username}</h1>
+}
